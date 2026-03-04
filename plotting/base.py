8e76e2bfb3ce6b32a87825e22643f6dd62f67d75
Colm Connaughton <colm.connaughton@zonlab.com>
2026-03-04 18:02:11 +0000

    Add example plots using matplotlib style elements from the textbook.

--- plotting/base.py
+++ plotting/base.py
@@ -53,3 +53,75 @@ def apply_tweaks(config, fig, ax):
     size = fig.get_size_inches()[1]
     fig.set_size_inches(size/ratio, size)
     return fig, ax
+
+
+def calculate_axis_ticks(ax, cz, mode='log', axis='y'):
+    if axis == 'y':
+        minval, maxval = ax.get_ylim()
+        ticks = ax.get_yticks()
+        minor_ticks = ax.get_yticks(minor=True)
+        ticklabels = ax.get_yticklabels()
+    elif axis == 'x':
+        minval, maxval = ax.get_xlim()
+        ticks = ax.get_xticks()
+        minor_ticks = ax.get_xticks(minor=True)
+        ticklabels = ax.get_xticklabels()
+    else:
+         print("calculate_axis_ticks(): axis must be x or y")
+         exit()
+
+    #print([mode, axis])
+    eps = 0.1*(maxval-minval)
+    if mode == 'log':
+        # This is for a log scale
+        data_range = int(round(maxval-minval))
+        step = max([1,int((data_range - (data_range%6))/6)])
+        ticks = [ int(round(v)) for v in np.arange(minval, maxval+eps, step)]
+        # If rounding results in ticks not spanning the data range, add extra ticks
+        if ticks[0] > minval:
+            ticks.insert(0,ticks[0]-step)
+        if ticks[-1] < maxval:
+            ticks.append(ticks[-1]+step)
+        # Generate the labels as strings
+        ticklabels = [r"$10^{{{0}}}$".format(tick) for tick in ticks]
+        # Replace 10^0 and 10^1 by 1 and 10 respectively
+        ticklabels = [label.replace('10^{0}', '1') for label in ticklabels]
+        ticklabels = [label.replace('10^{1}', '10') for label in ticklabels]
+        # Now generate the minor ticks
+        minor_ticks = []
+        for i in range(min(ticks), max(ticks)):
+            for j in range(2,10):
+                minor_ticks.append(i+np.log10(j))
+
+    elif mode == 'linear':
+        # This is for a linear scale
+        print("calculate_axis_ticks(): mode = 'linear' mode not implemented yet!")
+    elif mode == 'uniform':
+        # This is for uniformly spaced ticks on arbitrarily transformed axis
+        print("calculate_axis_ticks(): mode = 'uniform' not implemented yet!")
+    else:
+        print("calculate_axis_ticks(): Error - unknown mode, ", mode)
+
+    return ticks, ticklabels, minor_ticks
+
+
+def tplot(x, y, cx, cy, ax, label=None, xticks='linear', yticks='log', minorticks=True, **kwargs):
+    # Transform the data and add it to the axis
+    xt = list(map(cx, x))
+    yt = list(map(cy, y))
+    ax.plot(xt, yt, label=label, **kwargs)
+
+    # Set the ticks for the y axis
+    ticks, ticklabels, minor_ticks = calculate_axis_ticks(ax, cy, mode=yticks, axis='y')
+    ax.set_yticks(ticks)
+    ax.set_yticklabels(ticklabels)
+    if minorticks:
+        ax.set_yticks(minor_ticks, minor = True)
+
+    # Set the ticks for the x axis
+    ticks, ticklabels, minor_ticks = calculate_axis_ticks(ax, cx, mode=xticks, axis='x')
+    ax.set_xticks(ticks)
+    ax.set_xticklabels(ticklabels)
+    if minorticks:
+        ax.set_xticks(minor_ticks, minor = True)
+    return ax
